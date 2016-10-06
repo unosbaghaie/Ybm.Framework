f36f59ea00c77e0d273847e5f9fa9748e1118e22
unos.bm65@gmail.com <unos.bm65@gmail.com>
2016-10-06 10:23:51 +0330

    2

--- Ybm.CodeGenerator/Program.cs
+++ Ybm.CodeGenerator/Program.cs
@@ -14,6 +14,7 @@ namespace Ybm.CodeGenerator
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
