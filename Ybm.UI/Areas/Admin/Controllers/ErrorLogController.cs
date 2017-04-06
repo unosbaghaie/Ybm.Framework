7eaa944a84e66d347794928c7bf8d9c4d857c7ba
younos baghaee <unos.bm65@gmail.com>
2017-04-06 11:20:27 +0430

    IFilterable Functions

--- Ybm.UI/Areas/Admin/Controllers/ErrorLogController.cs
+++ Ybm.UI/Areas/Admin/Controllers/ErrorLogController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
+using Ybm.Common.Models;
+using Ybm.Framework;
 
 namespace Ybm.UI.Areas.Admin.Controllers
 {
@@ -13,5 +16,40 @@ namespace Ybm.UI.Areas.Admin.Controllers
         {
             return View();
         }
+
+
+
+
+        #region [IFilterable Functions]
+        public List<CustomFilterDescriptor> FilterData
+        {
+            get
+            {
+                ErrorLog q = new ErrorLog();
+                List<CustomFilterDescriptor> decriptors = Framework.ExpressionHelper.
+                    ExpressionBuilder.GetFilterFields<ErrorLog>(
+                    new Tuple<Expression<Func<object>>, string>(() => q.Id, "شناسه"),
+                    new Tuple<Expression<Func<object>>, string>(() => q.CreationDateTime, "تاریخ ثبت"),
+                    new Tuple<Expression<Func<object>>, string>(() => q.Message, "توضیحات")
+                    );
+
+                return decriptors;
+
+
+            }
+            set { }
+        }
+        public ActionResult FilterThePage(Dictionary<string, string> selectedFilters)
+        {
+            var filterData = FilterData;
+            filterData.ForEach(q => q.Member = q.Member.Replace("FilterField_", ""));
+            Framework.ExpressionHelper.ExpressionBuilder.SyncFilterData(filterData, selectedFilters);
+            var predicate = Framework.ExpressionHelper.ExpressionBuilder.MakeTheExpression<ErrorLog>(filterData);
+            TempData["predicate"] = predicate;
+            return Json(new { result = true });
+        }
+
+
+        #endregion
     }
 }
