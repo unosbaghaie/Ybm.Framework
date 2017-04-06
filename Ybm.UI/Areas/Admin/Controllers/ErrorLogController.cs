a7f1f976fcdb89af025aca56332e9f95864c0712
younos baghaee <unos.bm65@gmail.com>
2017-04-06 11:28:31 +0430

    ErrorLogMapper

--- Ybm.UI/Areas/Admin/Controllers/ErrorLogController.cs
+++ Ybm.UI/Areas/Admin/Controllers/ErrorLogController.cs
@@ -1,19 +1,38 @@
-﻿using System;
+﻿using Kendo.Mvc.UI;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
+using Ybm.Business;
 using Ybm.Common.Models;
 using Ybm.Framework;
+using Ybm.Framework.ExpressionHelper;
+using Ybm.UI.Infrastructure.Authorization;
+using Ybm.UI.Areas.Admin.Requirements;
+
 
 namespace Ybm.UI.Areas.Admin.Controllers
 {
     public class ErrorLogController : Controller
     {
+
+        IErrorLogBusiness errorLogBiz = ServiceFactory.CreateInstance<IErrorLogBusiness>();
+        
+
+
         // GET: Admin/ErrorLog
         public ActionResult Index()
         {
+
+            #region [IFilterable View Data]
+            ViewBag.filterControlsModel = FilterData;
+            //ViewBag.User_Id = User_Id;
+            #endregion
+
+
             return View();
         }
 
@@ -51,5 +70,31 @@ namespace Ybm.UI.Areas.Admin.Controllers
 
 
         #endregion
+
+
+
+        [ClaimBasedAuthorzation(TokenName = "پر کردن گیرید شکایات کاربر", ClaimType = "UserRight")]
+        public virtual ActionResult Read([DataSourceRequest] DataSourceRequest request, byte? status = null)
+        {
+            Expression<Func<ErrorLog, bool>> basePredicate = q => q.Id == 10;
+
+
+            #region [IFilterable Predicate]
+            if (TempData["predicate"] != null)
+                basePredicate = basePredicate.And((Expression<Func<ErrorLog, bool>>)TempData["predicate"]);
+            #endregion
+
+            IPagedList<ErrorLog> list;
+            list = errorLogBiz.FetchMulti(
+                request.Page,
+                request.PageSize,
+                q => q.Id, basePredicate,
+                false);
+
+            var count = errorLogBiz.Count(basePredicate);
+            return Json(list.ToList().Select(q => q.GetViewModel()).ToDataSourceResult(request, count), JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
---
+++ Ybm.UI/Areas/Admin/Requirements/ErrorLogMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ybm.Common.Models;
+using Ybm.UI.Areas.Admin.Requirements.ViewModels;
+
+namespace Ybm.UI.Areas.Admin.Requirements
+{
+    public static class ErrorLogMapper
+    {
+
+        public static ErrorLogViewModel GetViewModel(this ErrorLog model)
+        {
+
+
+
+
+
+
+
+
+            return new ErrorLogViewModel();
+        }
+
+
+    }
+}
